450db27c71c9ef5d8b82c1257640c36047a30b6d
Anubhav Sinha <email@anubhavsinha.com>
2012-09-21 04:04:52 +0530

    clean

--- utility.py
+++ utility.py
@@ -1,13 +1,19 @@
 #cloudy-ngram/utility.py
-#start with scraping google for number of results return for a typical query
-#step1: visit google search url with the query string
+
 import urllib2
 import re
 import math
 
+
+
 memcached = {}# this to be replaced with an actual memcached server
 
+
+
 def doc_frequency(ngram):
+    """
+    returns the number of documents from the web, which contains a particular ngram
+    """
     if ngram in memcached:
         return memcached.get(ngram)
     
@@ -16,11 +22,8 @@ def doc_frequency(ngram):
     #https://www.google.co.in/search?q=a&ie=utf-8&oe=utf-8&aq=t&rls=org.mozilla:en-GB:official&client=firefox-a
     request = urllib2.Request('https://www.google.co.in/search?q="%s"&ie=utf-8&oe=utf-8&aq=t&rls=org.mozilla:en-GB:official&client=firefox-a'%query)
     request.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:15.0) Gecko/20100101 Firefox/15.0.1')
-    #more headers may be set, like gzip deflate etc.
     response = urllib2.urlopen(request)
     doc = response.read()
-    #f = open("dump.html","w")
-    #f.write(doc)
     
     #structure to scrape
     #<div id=resultStats>About 554,000,000 results<nobr>  (0.17 seconds)&nbsp;</nobr></div>
@@ -28,10 +31,9 @@ def doc_frequency(ngram):
     match = re.search(regex,doc)
     result = match.group(1)
     result = float(result.replace(',',''))
+    
     memcached[ngram] = result
     return result
-
-#print doc_frequency("the")
 
 
 
@@ -41,12 +43,12 @@ def ngram_IDF(ngram):
     relevancy = term frequency * log (1 / document frequency)
     This function returns log (1 / document frequency)
     """
-    total_no_of_web_pages = 25270000000.0#number of pages returned by searching for "the" or "a"
+    total_no_of_web_pages = 25270000000.0 # number of pages returned by searching for "the" or "a"
     return math.log(total_no_of_web_pages/doc_frequency(ngram),10)
     
 
-print ngram_IDF("ancient polymath")#cold
-print ngram_IDF("ancient polymath")#hot
+print ngram_IDF("ancient polymath")# cold from google
+print ngram_IDF("ancient polymath")# hot from cache
 
 #to-do
 #explore octopy map-reduce implementation
