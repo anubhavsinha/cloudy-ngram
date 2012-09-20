25d7c0741180c44a4a446eb31f615334548c9d17
Anubhav Sinha <email@anubhavsinha.com>
2012-09-21 05:01:32 +0530

    Heroku or GAE

--- utility.py
+++ utility.py
@@ -4,7 +4,13 @@ import urllib2
 import re
 import math
 
-memcached = {}# this to be replaced with an actual memcached server
+memcached = {}# this to be replaced
+#with an actual high availability memcached server
+#eg. google app engine, heroku, aws
+#further dev, NLTK may be needed,
+#but GAE doesn't support NLTK
+#AWS is not free
+#Heroku wins!
 
 def doc_frequency(ngram):
     """
