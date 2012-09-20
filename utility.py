bbcc2c412cb9a3962d8d18a435e7192dce59f443
Anubhav Sinha <email@anubhavsinha.com>
2012-09-21 03:34:16 +0530

    spoofing as firefox top right search bar

--- utility.py
+++ utility.py
@@ -8,9 +8,10 @@ import math
 def doc_frequency(ngram):
     words = ngram.split()
     query = '+'.join(words)
-    request = urllib2.Request("http://google.com/search?q="+'"'+query+'"')
+    #https://www.google.co.in/search?q=a&ie=utf-8&oe=utf-8&aq=t&rls=org.mozilla:en-GB:official&client=firefox-a
+    request = urllib2.Request('https://www.google.co.in/search?q="%s"&ie=utf-8&oe=utf-8&aq=t&rls=org.mozilla:en-GB:official&client=firefox-a'%query)
     request.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:15.0) Gecko/20100101 Firefox/15.0.1')
-    #more headers to set, to fool google
+    #more headers may be set, like gzip deflate etc.
     response = urllib2.urlopen(request)
     doc = response.read()
     f = open("dump.html","w")
@@ -33,8 +34,8 @@ def ngram_IDF(ngram):
     relevancy = term frequency * log (1 / document frequency)
     This function returns log (1 / document frequency)
     """
-    total_no_of_web_pages = doc_frequency("a")#number of pages returned by searching for "the"
+    total_no_of_web_pages = 25270000000.0#number of pages returned by searching for "the" or "a"
     return math.log(total_no_of_web_pages/doc_frequency(ngram),10)
     
 
-print ngram_IDF("the")
+print ngram_IDF("ancient polymath")
